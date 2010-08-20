d591d88c2a6afa5d18c078ae95e3d4e2c9fe9080
Hugo Duncan <hugo_duncan@yahoo.com>
2010-08-19 22:43:15 -0400

    Update to clojure 1.2.0

--- project.clj
+++ project.clj
@@ -3,14 +3,12 @@
   :url "http://github.com/hugpduncan/atticus"
   :source-path "src/main/clojure"
   :test-path "src/test/clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
-                     [autodoc "0.7.0"]]
-  :repositories [["build.clojure.org" "http://build.clojure.org/releases/"]
-		 ["clojars.org" "http://clojars.org/repo/"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [autodoc "0.7.1"]]
   :autodoc {:name "Atticus"
-	    :description "Atticus is a test helper library."
-	    :copyright "Copyright Hugo Duncan 2010. All rights reserved."
-	    :web-src-dir "http://github.com/hugoduncan/atticus/blob/"
-	    :web-home "http://hugoduncan.github.com/atticus/" })
+            :description "Atticus is a test helper library."
+            :copyright "Copyright Hugo Duncan 2010. All rights reserved."
+            :web-src-dir "http://github.com/hugoduncan/atticus/blob/"
+            :web-home "http://hugoduncan.github.com/atticus/" })
